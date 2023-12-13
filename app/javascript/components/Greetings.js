aac4cf0c2727e2f424a8a2fb3bf8343547b55cff
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-12-12 21:54:11 -0300

    add greeting

--- app/javascript/components/Greetings.js
+++ app/javascript/components/Greetings.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSalute } from '../redux/slices/greetingSlice';
 
-console.log('3')
+console.log(3);
 
 const Greetings = () => {
-  return <div>Greetings</div>;
+  const dispatch = useDispatch();
+  const { greeting, loading } = useSelector((store) => store.greetingStore);
+
+  useEffect(() => {
+    dispatch(fetchSalute());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>{greeting || 'No greeting available'}</p>
+      )}
+    </div>
+  );
 };
 
 export default Greetings;
